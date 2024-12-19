e62ea2cb3823fa45ea89092d437611f936f30081
Bhabani2005 <135462975+Bhabani2005@users.noreply.github.com>
2024-12-19 10:46:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv("C:/Users/shakti/Downloads/ITC.NS (1).csv")
+  df = pd.read_csv("C:/Users/shakti/Downloads/ITC.NS.csv")
   df
 
   st.write('**X**')
