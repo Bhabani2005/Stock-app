79be5ab5b7a5aa83bbcdced3e18aebd96020f366
Bhabani2005 <135462975+Bhabani2005@users.noreply.github.com>
2024-12-19 23:33:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score 
 import math
 
 # Title
